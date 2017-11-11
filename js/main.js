6350fcb40f784ef83107dc2c04b8dd93098968ff
Month7 <31988780+Month7@users.noreply.github.com>
2017-11-11 23:39:27 +0800

    2017/11/11
    增加了商品购买页面
    商品购买页面增加了商品飞进购物车效果

--- js/main.js
+++ js/main.js
@@ -1,4 +1,208 @@
 $(function(){
+    /*=============================================商品飞入购物车效果===============================*/
+    $("#addCart").click(function(){
+        var imgSrc=$(".big").attr("src"); 
+        if($(".normal").length>0){
+            var $rbCart=$(".rb-cart");
+            var endLeft=$rbCart.offset().left;
+            var endTop=$rbCart.offset().top;
+        }
+        var flyer=$("<img class='flyer-img' src="+imgSrc+">");
+        flyer.fly({   
+            start: {   
+                left: event.pageX,//抛物体起点横坐标   
+                top: event.pageY //抛物体起点纵坐标   
+            },   
+            end: {   
+                left: endLeft,//抛物体终点横坐标   
+                top: endTop, //抛物体终点纵坐标   
+            },   
+            onEnd: function() {   
+                // $("#tip").show().animate({width: '200px'},300).fadeOut(500);////成功加入购物车动画效果   
+                // this.destory(); //销毁抛物体   
+                $(this).remove();
+            }   
+        });   
+    })
+     /*==================================产品界面放大镜效果=======================================*/
+     $.fn.fangDa = function(){
+        var that = $(this);
+        $imgNormal=that.find(".normal"); //正常图片容器
+        $Img=$imgNormal.find("img");
+        $Drag=that.find(".huaKuai");             //滑块容器
+        $show=that.find(".fangDa");              //放大图片容器
+        $showImg=$show.find("img");
+        num=$show.width()/$Drag.width();
+        $imgNormal.mousemove(function(e){
+            $Drag.css("display","block");  
+            $show.css("display","block");
+            var iX=e.pageX-$(this).offset().left-$Drag.width()/2;
+            var iY=e.pageY-$(this).offset().top-$Drag.height()/2;
+            var MaxX=$imgNormal.width()-$Drag.width();
+            var MaxY=$imgNormal.height()-$Drag.height();
+            iX = iX > 0 ? iX : 0;  
+            iX = iX < MaxX ? iX : MaxX;  
+            iY = iY > 0 ? iY : 0;  
+            iY = iY < MaxY ? iY : MaxY;    
+            $showImg.css("width",num*$imgNormal.width());
+            $showImg.css("height",num*$imgNormal.height());
+            $Drag.css({left:iX+"px",top:iY+"px"});            
+            $showImg.css({marginLeft:-num*iX+'px',marginTop:-num*iY+'px'});  
+        });
+        $imgNormal.mouseout(function(){  
+            $Drag.css("display","none");  
+            $show.css("display","none");  
+        });  
+    };
+    $("#fangdajing").fangDa();  
+    /*=========================================购物车交互================================*/
+    var $button1=$("button.title-button");
+    var $button2=$("button.bottom-button");
+    var $radioCheck=$("img.radio-check");
+    var $radiosCheck=$("img.radios-check");
+    function formatMoney(num){                       //以千进制格式化金额
+        num = num.toString().replace(/\$|\,/g,'');  
+        if(isNaN(num))  
+            num = "0";  
+        sign = (num == (num = Math.abs(num)));  
+        num = Math.floor(num*100+0.50000000001);  
+        cents = num%100;  
+        num = Math.floor(num/100).toString();  
+        if(cents<10)  
+        cents = "0" + cents;  
+        for (var i = 0; i < Math.floor((num.length-(1+i))/3); i++)  
+        num = num.substring(0,num.length-(4*i+3))+','+  
+        num.substring(num.length-(4*i+3));  
+        return (((sign)?'':'-') + num + '.' + cents);  
+    }
+    function isAny(){
+        var isAny=false;
+        $radioCheck.each(function(){
+            if("isselected"==$(this).attr("isselected")){
+                isAny=true;
+            }
+        })
+        if(isAny){
+            $button1.css("background-color","#ff9900");
+            $button1.css("border-color","#ff9900");
+            $button2.css("background-color","#ff9900");
+            $button2.removeAttr("disabled");
+        }
+        else{
+            $button1.css("border-color","#AAAAAA");
+            $button1.css("background-color","#AAAAAA");
+            $button2.css("background-color","#AAAAAA");
+            $button2.css("disabled","disabled");
+        }
+    }
+    function checkIsAll(){
+        var isAll=true;
+        $radioCheck.each(function(){
+            if("false"==$(this).attr("isselected")){
+                isAll=false;
+            }
+        });
+        if(isAll){
+            $radiosCheck.attr("isselect","isselect");
+            $radiosCheck.attr("src","images/cartSelected.png");
+        }
+        else{
+            $radiosCheck.attr("isselect","false");
+            $radiosCheck.attr("src","images/cartNotSelected.png");
+        }
+    }
+    $radioCheck.click(function(){
+        var $that=$(this);
+        var isselected=$that.attr("isselected");
+        if(isselected=="isselected"){
+            $that.attr("isselected","false");
+            $that.attr("src","images/cartNotSelected.png");
+            $that.parents("tr.cart-item").css("background-color","#fff");
+        }
+        else{
+            $that.attr("isselected","isselected");
+            $that.attr("src","images/cartSelected.png");
+            $that.parents("tr.cart-item").css("background-color","#FFF8E1");
+        }
+        isAny();
+        cal();
+        checkIsAll();
+    })  
+    $radiosCheck.click(function(){
+        var isselect=$(this).attr("isselect");
+        if(isselect=="isselect"){
+            $(this).attr("isselect","false");
+            $(this).attr("src","images/cartNotSelected.png");
+            $radioCheck.each(function(){
+                $(this).attr("isselected","false");
+                $(this).attr("src","images/cartNotSelected.png");
+                $(this).parents("tr.cart-item").css("background-color","#fff");
+            })
+        }
+        else{
+            $(this).attr("isselect","isselect");
+            $(this).attr("src","images/cartSelected.png");
+            $radioCheck.each(function(){
+                $(this).attr("isselected","isselected");
+                $(this).attr("src","images/cartSelected.png");
+                $(this).parents("tr.cart-item").css("background-color","#FFF8E1");
+            })
+        }
+        isAny();
+        cal();
+        checkIsAll();
+    })
+    function cal(){
+        var sumPrice=0;
+        var totalNumber=0;
+        $("img.radio-check[isselected='isselected']").each(function(){
+            var oid=$(this).attr("oid");
+            var price=$(".nowPrice[oid="+oid+"]").text();
+            var num=$("input.text[oid="+oid+"]").val();
+            price=price.replace(/￥/g,"");
+            price=price*num;
+            sumPrice+=new Number(price);
+            totalNumber+=new Number(num);
+        });
+        $("span.bottom-price,span.title-price").html("￥"+sumPrice);
+        $("span.total").html(totalNumber);
+    }
+    $("a.increase").click(function(){
+        var pid=$(this).attr("pid");
+        var oid=$(".text[oid="+pid+"]").attr("oid");
+        var num=$(".text[oid="+oid+"]").val();
+        num++;
+        $(".text[oid="+oid+"]").val(num);
+        isAny();
+        cal();
+        checkIsAll();
+        var oid_=$("img.radio-check[oid="+oid+"]").attr("oid");
+        var num_=$("input.text[oid="+oid_+"]").val();
+        var price_=$(".nowPrice[oid="+oid_+"]").text();
+        price_=price_.replace(/￥/g,"");
+        price_=price_*num_;
+        var smallPrice=new Number(price_);
+        smallPrice=formatMoney(smallPrice);
+        $("span.calPrice[oid="+oid+"]").html("￥"+smallPrice);
+    });
+    $("a.decrease").click(function(){
+        var pid=$(this).attr("pid");
+        var oid=$(".text[oid="+pid+"]").attr("oid");
+        var num=$(".text[oid="+oid+"]").val();
+        num--;
+        $(".text[oid="+oid+"]").val(num);
+        isAny();
+        cal();
+        checkIsAll();
+        var oid_=$("img.radio-check[oid="+oid+"]").attr("oid");
+        var num_=$("input.text[oid="+oid_+"]").val();
+        var price_=$(".nowPrice[oid="+oid_+"]").text();
+        price_=price_.replace(/￥/g,"");
+        price_=price_*num_;
+        var smallPrice=new Number(price_);
+        smallPrice=formatMoney(smallPrice);
+        $("span.calPrice[oid="+oid+"]").html("￥"+smallPrice);
+    });
     var i=0;
     var $imgH=$(".carousel img").css("height");
     var windowHeight=$(window).height();
@@ -40,14 +244,34 @@ $(function(){
     $(".user").mouseout(function(){
         $(".rbl-user").hide();
     });
+    $(".rs-like").mouseover(function(){
+        $(".rbl-love").show();
+    });
+   
+    $(".rs-like").mouseout(function(){
+        $(".rbl-love").hide();
+    });
+   
+    $(".rs-money").mouseover(function(){
+        $(".rbl-hurt").show();
+    });
+   
+    $(".rs-money").mouseout(function(){
+        $(".rbl-hurt").hide();
+    });
+   
+
     /*=================================浮动导航栏=============================================*/
     var $flowSearch=$(".hide-nav");
     var $flowNav=$(".floatled");
-    var fruit=$("#fruit").offset().top;
-    var fish=$("#fish").offset().top;
-    var rice=$("#rice").offset().top;
-    var tea=$("#tea").offset().top;
-    var footer=$(".footer").offset().top;
+  
+    if($("#fruit").length>0){
+        var fruit=$("#fruit").offset().top;
+        var fish=$("#fish").offset().top;
+        var rice=$("#rice").offset().top;
+        var tea=$("#tea").offset().top;
+        var footer=$(".footer").offset().top;
+    }
     var $htmlBody=$("html,body");
     fruit-=40;
     fish-=40;
@@ -129,10 +353,42 @@ $(function(){
         hideProduct(xid);
     });
     /*============================================================================================*/
-    var $height=$(".show-item").css("height");
-    var $width=$(".show-item").css("width");
-    $height=$height.replace(/px/,"");
-    $height/=2;
-    $(".show-content").css("height",$height+"px");
-    $(".show-content").css("width",$width);
+    if($(".show-item").length>0){
+        var $height=$(".show-item").css("height");
+        var $width=$(".show-item").css("width");
+        $height=$height.replace(/px/,"");
+        $height/=2;
+        $(".show-content").css("height",$height+"px");
+        $(".show-content").css("width",$width);
+    }
+  
+   
+    /*====================================缩略图效果=====================================*/
+    var $imgSmall=$("img.small");
+    $imgSmall.mouseenter(function(){
+        var bigUrl=$(this).attr("bigUrl");
+        $("img.big").attr("src",bigUrl);
+    });
+    $("img.big").load(function(){
+        $imgSmall.each(function(){
+            var url=$(this).attr("bigUrl");
+            img =new Image();
+            img.src=url;
+            img.onload=function(){
+                $("div.img4load").append($(img));
+            }
+        })
+    });
+
 });
+
+
+// var buy = 0;
+// var money = 2;
+// do {
+//     buy++;
+//     console.log(buy);
+//     money--;
+//     if (money == 0)
+//         break;
+// } while (buy);
