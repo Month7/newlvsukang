f236a8ef562c8aac056596ec1a79ddb9a4f53e7b
Month7 <31988780+Month7@users.noreply.github.com>
2017-11-22 17:31:09 +0800

    产品展示页面移动端

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//rem单位
 changeRootFont();
 function changeRootFont() {
     document.documentElement.style.fontSize=((window.innerWidth / 750) * 100) + 'px';
@@ -394,6 +395,7 @@ $(function(){
                 $itemHeight+"px"
             });
             $(".row").css("height",$productHeight);
+            
             $(".sortProduct[xid=1]").show().siblings().hide();
             
             $(".drop-item").click(function(){
@@ -499,15 +501,55 @@ $(function(){
         
     })
     /*====================================移动端适配================================*/
+    //全部分类页面
     if($(window).width()<=768){
+          //购物车页面
         $(".cart-item").find("td").each(function(){
             if($(this).find("a").text()=="删除"){
                 if($(this).attr("class")!="mobie-item"){
                     $(this).hide();
                 }     
             }
+        });
+        var $wHeight=$(window).height();
+        var $wWidth=$(window).width();
+       
+        
+        var dcH=$(document).height();  
+        var dcH2;
+        var dcH3;
+        var dcH4;
+        /*=====================上拉加载=================*/
+        $(window).scroll(function(){
+            if($(window).scrollTop() + $(window).height() >=dcH-100){
+                setTimeout(function(){
+                        $("#row3").show();  
+                        dcH2=$(document).height();
+                        console.log("dcH2="+dcH2);
+                },1000);
+            }
+            if($(window).scrollTop() + $(window).height() >=dcH2-50){
+                setTimeout(function(){
+                        $("#row4").show();  
+                        dcH3=$(document).height();  
+                        console.log("dcH3="+dcH3);
+                },1000);
+            }
+            if($(window).scrollTop() + $(window).height() >=dcH3-50){
+                setTimeout(function(){
+                        $("#row5").show();  
+                        dcH4=$(document).height();  
+                        console.log("dcH4="+dcH4);
+                },1000);
+            }
+            if($(window).scrollTop() + $(window).height() >=dcH4-100){
+                setTimeout(function(){
+                        $("#row6").show();  
+                        $(".mobie-slide").text("我也是有底线的");
+                        $("#spF").show();
+                },1000);
+            }
         })
     }
-   
 });
 
